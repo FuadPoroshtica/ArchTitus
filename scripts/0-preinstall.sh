67e04dfd13ce6e120ee1feedebe41b594d7faae0
Fuad Poroshtica <fuad23@ru.is>
2023-09-23 00:49:59 +0000

    add dual booting w/windows

--- scripts/0-preinstall.sh
+++ scripts/0-preinstall.sh
@@ -25,6 +25,11 @@ echo -ne "
 
 Setting up mirrors for optimal download
 "
+echo "Choose installation type:"
+echo "1) Full clean install"
+echo "2) Dual-boot with Windows"
+read -p "Enter your choice (1/2): " install_type
+
 source $CONFIGS_DIR/setup.conf
 iso=$(curl -4 ifconfig.co/country-iso)
 timedatectl set-ntp true
@@ -52,22 +57,30 @@ echo -ne "
                     Formating Disk
 -------------------------------------------------------------------------
 "
-umount -A --recursive /mnt # make sure everything is unmounted before we start
-# disk prep
-sgdisk -Z ${DISK} # zap all on disk
-sgdisk -a 2048 -o ${DISK} # new gpt disk 2048 alignment
+if [[ "$install_type" == "1" ]]; then  # Full clean install
+    if [[ "${FS}" == "btrfs" ]]; then
+        mkfs.vfat -F32 -n "EFIBOOT" ${partition2}
+        mkfs.btrfs -L ROOT ${partition3} -f
+        mount -t btrfs ${partition3} /mnt
+        subvolumesetup
+    # ... (rest of your logic for ext4 and luks)
+    mkdir -p /mnt/boot/efi
+    mount -t vfat -L EFIBOOT /mnt/boot/efi
 
-# create partitions
-sgdisk -n 1::+1M --typecode=1:ef02 --change-name=1:'BIOSBOOT' ${DISK} # partition 1 (BIOS Boot Partition)
-sgdisk -n 2::+300M --typecode=2:ef00 --change-name=2:'EFIBOOT' ${DISK} # partition 2 (UEFI Boot Partition)
-sgdisk -n 3::-0 --typecode=3:8300 --change-name=3:'ROOT' ${DISK} # partition 3 (Root), default start, remaining
-if [[ ! -d "/sys/firmware/efi" ]]; then # Checking for bios system
-    sgdisk -A 1:set:2 ${DISK}
-fi
-partprobe ${DISK} # reread partition table to ensure it is correct
-
-# make filesystems
-echo -ne "
+elif [[ "$install_type" == "2" ]]; then  # Dual-boot with Windows
+    if [[ "${FS}" == "btrfs" ]]; then
+        mkfs.btrfs -L ROOT ${partition3} -f
+        mount -t btrfs ${partition3} /mnt
+        subvolumesetup
+    # ... (rest of your logic for ext4 and luks)
+    # Mount the existing EFI partition to /mnt/boot/efi
+    # You'll need to identify your existing EFI partition
+    mkdir -p /mnt/boot/efi
+    mount -t vfat EXISTING_EFI_PARTITION /mnt/boot/efi
+else
+    echo "Invalid choice. Exiting."
+    exit 1
+fi"
 -------------------------------------------------------------------------
                     Creating Filesystems
 -------------------------------------------------------------------------
